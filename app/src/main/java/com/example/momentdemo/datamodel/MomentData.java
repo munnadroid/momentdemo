4401c2431453d2ae8aa829130f9a29fcfdecba1f
munnadroid <rajugotame@stuxbot.co.uk>
2014-07-11 10:57:16 +0545

    data model class added

--- app/src/main/java/com/example/momentdemo/datamodel/MomentData.java
+++ app/src/main/java/com/example/momentdemo/datamodel/MomentData.java
@@ -7,13 +7,27 @@ public class MomentData {
 
     private int id;
     private String username;
+    private String title;
+    private String avatarImageUrl;
+    private String backgroundImageUrl;
+    private int photoCount;
+    private int videoCount;
+    private int audioCount;
 
-    public MomentData(){
+
+    public MomentData() {
     }
 
-    public MomentData(int id, String username){
-        this.id=id;
-        this.username=username;
+    public MomentData(int id, String username, String title, String avatarImageUrl,
+                      String backgroundImageUrl, int photoCount, int videoCount, int audioCount) {
+        this.id = id;
+        this.username = username;
+        this.title = title;
+        this.avatarImageUrl = avatarImageUrl;
+        this.backgroundImageUrl = backgroundImageUrl;
+        this.photoCount = photoCount;
+        this.videoCount = videoCount;
+        this.audioCount = audioCount;
     }
 
     public int getId() {
@@ -30,5 +44,53 @@ public class MomentData {
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAvatarImageUrl() {
+        return avatarImageUrl;
+    }
+
+    public void setAvatarImageUrl(String avatarImageUrl) {
+        this.avatarImageUrl = avatarImageUrl;
+    }
+
+    public String getBackgroundImageUrl() {
+        return backgroundImageUrl;
+    }
+
+    public void setBackgroundImageUrl(String backgroundImageUrl) {
+        this.backgroundImageUrl = backgroundImageUrl;
+    }
+
+    public int getPhotoCount() {
+        return photoCount;
+    }
+
+    public void setPhotoCount(int photoCount) {
+        this.photoCount = photoCount;
+    }
+
+    public int getVideoCount() {
+        return videoCount;
+    }
+
+    public void setVideoCount(int videoCount) {
+        this.videoCount = videoCount;
+    }
+
+    public int getAudioCount() {
+        return audioCount;
+    }
+
+    public void setAudioCount(int audioCount) {
+        this.audioCount = audioCount;
     }
 }
