1cbf941c6cd0d2162087b45e80cbee4e677d0775
munnadroid <rajugotame@stuxbot.co.uk>
2014-07-11 16:35:00 +0545

    json key and other constans parameter added

---
+++ app/src/main/java/com/example/momentdemo/util/Constants.java
@@ -0,0 +1,24 @@
+package com.example.momentdemo.util;
+
+/**
+ * Created by munnadroid on 7/11/14.
+ */
+public class Constants {
+
+    public static final String KEY_MOMENTS="moments";
+    public static final String KEY_MOMENT_ID="id";
+    public static final String KEY_TITLE="title";
+    public static final String KEY_MOMENT_ITEMS="moment_items";
+    public static final String KEY_MOMENT_ITEMS_ID="id";
+    public static final String KEY_MOMENT_ITEMS_TYPE="item_type";
+    public static final String KEY_USER="user";
+    public static final String KEY_USER_ID="id";
+    public static final String KEY_USER_NAME="name";
+    public static final String KEY_USER_AVATAR="avatar";
+    public static final String KEY_BACKGROUND="background";
+
+    public static final String TAG_PHOTO_TYPE="photo";
+    public static final String TAG_VIDEO_TYPE="video";
+    public static final String TAG_AUDIO_TYPE="audio";
+
+}
